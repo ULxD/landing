ea3feaed2ec91e56dc2c86187c1304194dd37ca0
Whelisson Dias <whelidias@hotmail.com>
2022-01-31 06:53:00 -0300

    Styled home components

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,14 +31,14 @@ const Home = () => {
 
       <MainActions>
         <Link href='/register'>
-          <MainButton>
+          <ButtonExperiment>
           Experimentar
-          </MainButton>
+          </ButtonExperiment>
         </Link>
         <Link href='/about'>
-          <MainButton>
+          <ButtonAbout>
             Saiba Mais
-          </MainButton>
+          </ButtonAbout>
         </Link>
 
       </MainActions>
@@ -51,25 +51,100 @@ const Home = () => {
 
 export default Home
 
-export const PageContainer = styled.div`` 
+export const PageContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #ffffffbe;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+` 
 
-export const NavBar = styled.div``
+export const NavBar = styled.div`
+    max-width: 1440px;
+    width: 90%;
+    height: 15vh;
 
-export const Logo = styled.p``
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-export const Button = styled.button``
+`
+
+export const Logo = styled.p`
+    font-weight: bold;
+    cursor: pointer;
+    font-size: 18px;
+`
+
+export const Button = styled.button`
+    min-height: 45px;
+    min-width: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0px 15px;
+
+    border: none; 
+    outline: none;
+    cursor: pointer;
+
+    font: inherit;
+    font-size: 12px;
+
+    background-color: #333;
+    color: #ffffff;
+
+    transition: 0.5s;
+
+    &:hover {
+      color: #333; 
+      background-color: #ccc;
+      font-weight: bold;
+    }
+`
   
 export const ButtonNav = styled(Button)``
 
-export const MainButton = styled(Button)``
+export const ButtonExperiment = styled(Button)`
+    margin-right: 7.5px;
+`
+
+export const ButtonAbout = styled(Button)`
+    margin-left: 7.5px;
+    background-color: #333333a0;
+`
   
-export const Main = styled.main``
+export const Main = styled.main`
+    
+    max-width: 1440px;
+    width: 90%;
+    height: 85vh;
 
-export const HeadLine = styled.h1``
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 
-export const SubHeadLine = styled.h2``
+export const HeadLine = styled.h1`
+    font-size: 90px;
+    margin-bottom: 24px;
+`
 
-export const MainActions = styled.div``
+export const SubHeadLine = styled.h2`
+    font-size: 24px;
+    font-weight: normal;
+    text-align: center;
+
+    max-width: 70%;
+    margin-bottom: 24px;
+`
+
+export const MainActions = styled.div`
+    display: flex;
+`
 
 
   
